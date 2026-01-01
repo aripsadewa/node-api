3caa8c675f8b7e38e201567229f33a390e132d8d
aripsadewa <find.arip@gmail.com>
2026-01-01 21:53:41 +0700

    crud

--- controller/post.js
+++ controller/post.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const models = require("../models");
 
 function save(req, res) {
@@ -29,6 +30,24 @@ function show(req, res) {
 
   models.Post.findByPk(id)
     .then((result) => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({
+          message: "Post not found",
+        });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "something wrong",
+      });
+    });
+}
+
+function index(req, res) {
+  models.Post.findAll()
+    .then((result) => {
       res.status(200).json(result);
     })
     .catch((error) => {
@@ -38,7 +57,53 @@ function show(req, res) {
     });
 }
 
+function update(req, res) {
+  const id = req.params.id;
+  const updatedPost = {
+    title: req.body.title,
+    content: req.body.content,
+    imageUrl: req.body.image_url,
+    categoryId: req.body.categoryId,
+  };
+
+  const userId = 1;
+
+  models.Post.update(updatedPost, { where: { id: id, userId: userId } })
+    .then((result) => {
+      res.status(200).json({
+        message: "Update succesful",
+        post: updatedPost,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "something wrong",
+        error: error,
+      });
+    });
+}
+
+function destroy(req, res) {
+  const id = req.params.id;
+  const userId = 1;
+  models.Post.destroy({ where: { id: id, userId: userId } })
+    .then((result) => {
+      res.status(200).json({
+        message: "Delete succesful",
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "something wrong",
+        error: error,
+      });
+    });
+}
+
 module.exports = {
   save: save,
   show: show,
+  index: index,
+  update: update,
+  destroy: destroy,
 };
--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,8 @@ const router = express.Router();
 
 router.post("/", postController.save);
 router.get("/:id", postController.show);
+router.get("/", postController.index);
+router.patch("/:id", postController.update);
+router.delete("/:id", postController.destroy);
 
 module.exports = router;
